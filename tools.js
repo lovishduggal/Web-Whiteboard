fc9ffa37d4cf50a6d21c668d6c9b9b35591caa55
Lovish Duggal <lovishduggal11@gmail.com>
2024-06-18 09:25:28 +0530

    add listeners to pencil and eraser

--- tools.js
+++ tools.js
@@ -1,11 +1,15 @@
 const optionsCont = document.querySelector('.options-cont');
+let optionsFlag = true;
 const toolsCont = document.querySelector('.tools-cont');
 const pencilToolCont = document.querySelector('.pencil-tool-cont');
 const eraserToolCont = document.querySelector('.eraser-tool-cont');
-let optionsOpen = true;
+const pencil = document.querySelector('.pencil');
+const eraser = document.querySelector('.eraser');
+let pencilFlag = false;
+let eraserFlag = false;
 optionsCont.addEventListener('click', () => {
-    optionsOpen = !optionsOpen;
-    if (optionsOpen) openTools();
+    optionsFlag = !optionsFlag;
+    if (optionsFlag) openTools();
     else closeTools();
 });
 
@@ -21,7 +25,19 @@ function closeTools() {
     const iconElement = optionsCont.children[0];
     iconElement.classList.remove('fa-bars');
     iconElement.classList.add('fa-xmark');
-    toolsCont.style.display = 'none';
+    toolsCont.style.transform = 'translateX(-10rem)';
     pencilToolCont.style.display = 'none';
     eraserToolCont.style.display = 'none';
 }
+
+pencil.addEventListener('click', () => {
+    pencilFlag = !pencilFlag;
+    if (pencilFlag) pencilToolCont.style.display = 'block';
+    else pencilToolCont.style.display = 'none';
+});
+
+eraser.addEventListener('click', () => {
+    eraserFlag = !eraserFlag;
+    if (eraserFlag) eraserToolCont.style.display = 'flex';
+    else eraserToolCont.style.display = 'none';
+});
