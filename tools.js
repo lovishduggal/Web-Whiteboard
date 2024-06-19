9c2561e8cacb36c39eeb7c2a555224b6a17d64f8
Lovish Duggal <lovishduggal11@gmail.com>
2024-06-19 08:54:51 +0530

    add sticky-note drop and drag functionality

--- tools.js
+++ tools.js
@@ -58,4 +58,43 @@ sticky.addEventListener('click', (e) => {
                                 </div>`;
 
     document.body.appendChild(stickyCont);
+
+    stickyCont.onmousedown = function (e) {
+        dragAndDrop(stickyCont, e);
+    };
+    stickyCont.ondragstart = function () {
+        return false;
+    };
 });
+
+// Drag and Drop on sticky note
+function dragAndDrop(element, event) {
+    let shiftX = event.clientX - element.getBoundingClientRect().left;
+    let shiftY = event.clientY - element.getBoundingClientRect().top;
+
+    element.style.position = 'absolute';
+    element.style.zIndex = 1000;
+    document.body.append(element);
+
+    moveAt(event.pageX, event.pageY);
+
+    // moves the element at (pageX, pageY) coordinates
+    // taking initial shifts into account
+    function moveAt(pageX, pageY) {
+        element.style.left = pageX - shiftX + 'px';
+        element.style.top = pageY - shiftY + 'px';
+    }
+
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+    }
+
+    // move the element on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+
+    // drop the element, remove unneeded handlers
+    element.onmouseup = function () {
+        document.removeEventListener('mousemove', onMouseMove);
+        element.onmouseup = null;
+    };
+}
