4e3d615212fbcfb8a9bce75edab6e73bc14361b9
Lovish Duggal <lovishduggal11@gmail.com>
2024-06-19 07:44:24 +0530

    add event to sticky-note

--- tools.js
+++ tools.js
@@ -5,6 +5,7 @@ const pencilToolCont = document.querySelector('.pencil-tool-cont');
 const eraserToolCont = document.querySelector('.eraser-tool-cont');
 const pencil = document.querySelector('.pencil');
 const eraser = document.querySelector('.eraser');
+const sticky = document.querySelector('.sticky-note');
 let pencilFlag = false;
 let eraserFlag = false;
 optionsCont.addEventListener('click', () => {
@@ -40,4 +41,21 @@ eraser.addEventListener('click', () => {
     eraserFlag = !eraserFlag;
     if (eraserFlag) eraserToolCont.style.display = 'flex';
     else eraserToolCont.style.display = 'none';
+});
+
+sticky.addEventListener('click', (e) => {
+    const stickyCont = document.createElement('div');
+    stickyCont.setAttribute('class', 'sticky-cont');
+    stickyCont.innerHTML = `  <div class="header-cont">
+                                <div class="minimize">
+                                  <i class="fa-solid fa-down-left-and-up-right-to-center"></i>
+                                  <!-- <i class="fa-solid fa-up-right-and-down-left-from-center"></i> -->
+                                </div>
+                                <div class="remove"><i class="fa-solid fa-xmark"></i></div>
+                                 </div>
+                                <div class="note-cont">
+                                  <textarea></textarea>
+                                </div>`;
+
+    document.body.appendChild(stickyCont);
 });
