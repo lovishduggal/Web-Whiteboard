d47f06f038d6d76ecfd1e47b94020032de4152bc
Lovish Duggal <lovishduggal11@gmail.com>
2024-06-27 09:09:55 +0530

    add undo redo functionality

--- canva.js
+++ canva.js
@@ -6,6 +6,8 @@ const download = document.querySelector('.download');
 const pencilColor = document.querySelectorAll('.pencil-color');
 const pencilWidthEle = document.querySelector('.pencil-width');
 const eraserWidthEle = document.querySelector('.eraser-width');
+const redo = document.querySelector('.redo');
+const undo = document.querySelector('.undo');
 
 let mouseDown = false;
 
@@ -13,6 +15,9 @@ let penColor = 'blue';
 let eraserColor = 'white';
 let penWidth = pencilWidthEle.value;
 let eraserWidth = eraserWidthEle.value;
+
+let undoRedoStack = [];
+let undoRedoIndex = 0;
 
 // API:
 const ctx = canvas.getContext('2d');
@@ -27,20 +32,64 @@ canvas.addEventListener('mousedown', (e) => {
 
 canvas.addEventListener('mousemove', (e) => {
     if (mouseDown) {
-        drawLine({ x: e.clientX, y: e.clientY });
+        drawLine({
+            x: e.clientX,
+            y: e.clientY,
+            color: eraserFlag ? eraserColor : penColor,
+            width: eraserFlag ? eraserWidth : penWidth,
+        });
     }
 });
 
 canvas.addEventListener('mouseup', (e) => {
     mouseDown = false;
+    const dataURL = canvas.toDataURL();
+    undoRedoStack.push(dataURL);
+    undoRedoIndex = undoRedoStack.length - 1;
 });
 
+undo.addEventListener('click', (e) => {
+    if (undoRedoIndex > 0) {
+        undoRedoIndex--;
+    }
+    const trackObj = {
+        undoRedoIndex,
+        undoRedoStack,
+    };
+    undoRedoCanvas(trackObj);
+});
+
+redo.addEventListener('click', (e) => {
+    if (undoRedoIndex < undoRedoStack.length - 1) {
+        undoRedoIndex++;
+    }
+    const trackObj = {
+        undoRedoIndex,
+        undoRedoStack,
+    };
+    undoRedoCanvas(trackObj);
+});
+
+function undoRedoCanvas(trackObj) {
+    undoRedoIndex = trackObj.undoRedoIndex;
+    undoRedoStack = trackObj.undoRedoStack;
+    const dataURL = undoRedoStack[undoRedoIndex];
+    const img = new Image();
+    img.src = dataURL;
+    img.onload = () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
+}
 function beginPath(strokeObj) {
     ctx.beginPath();
     ctx.moveTo(strokeObj.x, strokeObj.y);
 }
 
 function drawLine(strokeObj) {
+    toolbar.strokeStyle = strokeObj.color;
+    toolbar.lineWidth = strokeObj.width;
     ctx.lineTo(strokeObj.x, strokeObj.y);
     ctx.stroke();
 }
@@ -54,6 +103,7 @@ pencilColor.forEach((color) => {
 pencilWidthEle.addEventListener('change', () => {
     penWidth = pencilWidthEle.value;
     ctx.lineWidth = penWidth;
+    ctx.strokeStyle = penColor;
 });
 
 eraserWidthEle.addEventListener('change', () => {
