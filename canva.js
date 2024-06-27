3bbee884f95d3345c0cdba9865e68f11fdf34803
Lovish Duggal <lovishduggal11@gmail.com>
2024-06-27 07:48:13 +0530

    add pencil with change and eraser functionality

--- canva.js
+++ canva.js
@@ -1,12 +1,22 @@
 const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const pencilColor = document.querySelectorAll('.pencil-color');
+const pencilWidthEle = document.querySelector('.pencil-width');
+const eraserWidthEle = document.querySelector('.eraser-width');
+
 let mouseDown = false;
+
+let penColor = 'blue';
+let eraserColor = 'white';
+let penWidth = pencilWidthEle.value;
+let eraserWidth = eraserWidthEle.value;
 
 // API:
 const ctx = canvas.getContext('2d');
-ctx.strokeStyle = 'blue';
-ctx.lineWidth = 3;
+ctx.strokeStyle = penColor;
+ctx.lineWidth = penWidth;
 
 // mouse down -> start new path, mousemove -> path fill
 canvas.addEventListener('mousedown', (e) => {
@@ -33,3 +43,29 @@ function drawLine(strokeObj) {
     ctx.lineTo(strokeObj.x, strokeObj.y);
     ctx.stroke();
 }
+pencilColor.forEach((color) => {
+    color.addEventListener('click', () => {
+        penColor = color.classList[0];
+        ctx.strokeStyle = penColor;
+    });
+});
+
+pencilWidthEle.addEventListener('change', () => {
+    penWidth = pencilWidthEle.value;
+    ctx.lineWidth = penWidth;
+});
+
+eraserWidthEle.addEventListener('change', () => {
+    eraserWidth = eraserWidthEle.value;
+    ctx.lineWidth = eraserWidth;
+});
+
+eraser.addEventListener('click', () => {
+    if (eraserFlag) {
+        ctx.strokeStyle = eraserColor;
+        ctx.lineWidth = eraserWidth;
+    } else {
+        ctx.strokeStyle = penColor;
+        ctx.lineWidth = penWidth;
+    }
+});
